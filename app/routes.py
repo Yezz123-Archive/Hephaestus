a08225e57ed7c43884c991244c5a5dec09eb0679
Yasser Tahiri <yasserth19@gmail.com>
2021-06-21 02:02:44 +0100

    Fix add the new routes :rocket:

--- app/routes.py
+++ app/routes.py
@@ -61,3 +61,70 @@ def fibonacci_invalid_path(invalid_path_parameter):
     db.session.commit()
 
     return response
+
+
+@app.route("/power/<x>/<y>")
+def power(x, y):
+    try:
+        x = float(x)
+        y = float(y)
+    except ValueError:
+        response = jsonify(
+            message=(
+                "Both numbers must be float or integer. Now x is {}, and y is {}".format(
+                    type(x), type(y)
+                )
+            )
+        )
+        response.status_code = 400
+
+        db.session.add(
+            RequestsModel(
+                request.url, str(json.loads(response.get_data().decode("utf-8")))
+            )
+        )
+        db.session.commit()
+
+        return response
+
+    response = jsonify(power=pow(x, y))
+    response.status_code = 200
+
+    db.session.add(
+        RequestsModel(request.url, str(json.loads(response.get_data().decode("utf-8"))))
+    )
+    db.session.commit()
+
+    return response
+
+
+@app.route("/factorial/<int:n>")
+def factorial(n):
+
+    response = jsonify(factorial=utils.factorial(n))
+    response.status_code = 200
+
+    db.session.add(
+        RequestsModel(request.url, str(json.loads(response.get_data().decode("utf-8"))))
+    )
+    db.session.commit()
+    return response
+
+
+@app.route("/factorial/<invalid_path_parameter>")
+def factorial_invalid_path(invalid_path_parameter):
+    response = jsonify(
+        message=(
+            "Parameter need to be integer. Now is {}".format(
+                type(invalid_path_parameter)
+            )
+        )
+    )
+    response.status_code = 400
+
+    db.session.add(
+        RequestsModel(request.url, str(json.loads(response.get_data().decode("utf-8"))))
+    )
+    db.session.commit()
+
+    return response
